6670abf41e4dccf2d49834ebf726b90f598761cb
MinKi <alsrl0023@naver.com>
2021-04-08 22:51:27 +0900

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('hi');
+// const express = require('express') // 모듈을 가져온다.
+import express from "express";
+const app = express()   // app을 만든다.express()를 실행한다.
+const port = 3000
+
+// function handleListening(){
+//     console.log(`Example app listening at http://localhost:${port}`)
+// }
+
+const handleListening = () => 
+    console.log(`Listening on: http://localhost:${port}`);
+
+// function handleProfile(req, res){
+//     res.send("You are on my profile");
+// }
+
+// Javascript arrow Function
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+
+app.get('/',function(req, res){
+    res.send('hellow world')
+})
+
+app.get("/profile",handleProfile);
+
+app.listen(port, handleListening)
