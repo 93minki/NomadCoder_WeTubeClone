8a267d95d8f2e4529789ec348a129a2dd06cea5e
MinKi <alsrl0023@naver.com>
2021-04-11 22:49:50 +0900

    middleware

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
 // const express = require('express') // 모듈을 가져온다.
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 const app = express()   // app을 만든다.express()를 실행한다.
-const port = 3000
+const PORT = 4000
 
-// function handleListening(){
-//     console.log(`Example app listening at http://localhost:${port}`)
-// }
 
 const handleListening = () => 
-    console.log(`Listening on: http://localhost:${port}`);
+    console.log(`Listening on: http://localhost:${PORT}`);
 
-// function handleProfile(req, res){
-//     res.send("You are on my profile");
-// }
+const handleHome = (req, res) => res.send("Hello from my Home");
 
-// Javascript arrow Function
 const handleProfile = (req, res) => res.send("You are on my profile");
 
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(helmet());
+app.use(morgan("dev"));
 
-app.get('/',function(req, res){
-    res.send('hellow world')
-})
+const middleware = (req,res,next)=>{
+    res.send('not happening');
+}
+``
+app.get("/",middleware, handleHome);
 
 app.get("/profile",handleProfile);
 
-app.listen(port, handleListening)
+app.listen(PORT, handleListening)
